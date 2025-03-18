02dc7dee1ac5fe6d45197d89a82e789f9d541b88
mxdxvz <lhufancia@gbox.adnu.edu.ph>
2025-03-18 11:52:57 +0800

    Add files via upload

---
+++ HufanciaN2Am/HufanciaN2Am/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HufanciaN2Am project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HufanciaN2Am.settings')
+
+application = get_wsgi_application()
