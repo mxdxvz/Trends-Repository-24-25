b4e8a4f60cf6f3731020034c341736b978545711
Leah Genyle E. Hufancia <lhufancia@gbox.adnu.edu.ph>
2025-05-14 02:06:15 +0800

    Delete directory directory

--- directory/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
-
-def home(request):
-    template = loader.get_template('directory.html')
-    return HttpResponse(template.render())
